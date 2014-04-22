3bb841667963ece45684fdaf37c4d9e05fc76a60
Yoshi Gillaspie <yoshi.gillaspie@gmail.com>
2014-04-22 10:39:56 -0700

    change scraper

--- scraper.rb
+++ scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
   3.times do |shape|
     3.times do |num|
       3.times do |shade|
-        card = color + shape + num |shade| + 1
+        card = (color * 27) + (shape * 9) + (num * 3) + shade + 1
         saver = "#{color}#{shape}#{num}#{shade}.png"
         site = "http://www.puzzles.setgame.com/images/setcards/#{card}.gif"
         open(saver, 'wb') do |file|
