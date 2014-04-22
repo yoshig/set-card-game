d7117d4b993914e41ea15bb998b64953b875b9bc
Yoshi Gillaspie <yoshi.gillaspie@gmail.com>
2014-04-22 09:42:15 -0700

    resave card images

--- scraper.rb
+++ scraper.rb
@@ -1,12 +1,16 @@
 require 'open-uri'
 
-82.times do |card|
-  next if card == 0
-  saver = "image#{card}.png"
-  site = "http://www.puzzles.setgame.com/images/setcards/#{card}.gif"
-  open(saver, 'wb') do |file|
-    file << open(site).read
+3.times do |color|
+  3.times do |shape|
+    3.times do |num|
+      3.times do |shade|
+        card = color + shape + num |shade| + 1
+        saver = "#{color}#{shape}#{num}#{shade}.png"
+        site = "http://www.puzzles.setgame.com/images/setcards/#{card}.gif"
+        open(saver, 'wb') do |file|
+          file << open(site).read
+        end
+      end
+    end
   end
 end
-
-http://www.puzzles.setgame.com/images/setcards/2.gif
