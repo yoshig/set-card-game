b1f7d4c5f61a7687fb4436df36d0abc6d3bdd141
Yoshi Gillaspie <yoshi.gillaspie@gmail.com>
2014-04-23 19:29:48 -0700

    Add beautiful cards to the beginning (UI(

--- game_ui.js
+++ game_ui.js
@@ -1,5 +1,24 @@
 (function(root) {
   var SetGame = root.SetGame = (root.SetGame || {})
-  
-  
+
+  GameUI = SetGame.GameUI = function() {
+  	this.game = new SetGame.Game()
+  	this.startGame();
+  }
+
+  GameUI.prototype.startGame = function() {
+  	this.game.startGame();
+  	this.game.tableCards.forEach(function(card) {
+  		var imageURL = "https://s3-us-west-1.amazonaws.com/setcardgame/card_images/" + card + ".png"
+  		var imageTag = "<img src=" + imageURL + ">"
+  		$('.gameboard').append(imageTag);
+  	});
+  }
+
+  SetGame.buildBoard = function() {
+  }
+
+  SetGame.chooseCard = function(card) {
+
+  }
 })(this)
